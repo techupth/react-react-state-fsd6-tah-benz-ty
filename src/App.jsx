8ffd7cb86d27030a25c90f0a836287d43cdf5c4c
Your Name <you@example.com>
2024-06-05 12:05:56 +0700

    feat: add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greet, greeting] = useState("");
+
   return (
     <div className="App">
       <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greet}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => greeting(`สวัสดี!`)}>สวัสดี!</button>
+        <button onClick={() => greeting(`Hi!`)}>Hi!</button>
+        <button onClick={() => greeting(`你好!`)}>你好!</button>
       </div>
     </div>
   );
